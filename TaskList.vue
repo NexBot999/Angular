c6ed6a75a19cde0a9e6b690ba65acd957ac12e8b
NexBot999 <80535259+NexBot999@users.noreply.github.com>
2023-08-01 14:37:30 +0200

    Create TaskList.vue

---
+++ TaskList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Ma liste de tâches</h1>
+    <input type="text" v-model="newTask" @keyup.enter="addTask">
+    <button @click="addTask">Ajouter</button>
+    <ul>
+      <li v-for="task in tasks" :key="task">{{ task }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tasks: ['Tâche 1', 'Tâche 2', 'Tâche 3'],
+      newTask: '',
+    };
+  },
+  methods: {
+    addTask() {
+      if (this.newTask.trim() !== '') {
+        this.tasks.push(this.newTask);
+        this.newTask = '';
+      }
+    },
+  },
+};
+</script>
